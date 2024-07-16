0564851b262cce2e847da831345fc1a322be871d
HazemUsama <hazemosama681@gmail.com>
2024-07-16 16:33:41 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,24 @@
 """Auth module for handling authentication
 """
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(email: str, password: str) -> User:
+        """Register a new user
+        """
+        if self._db.find_user_by(email=email):
+            raise ValueError(f"User {email} already exists")
+        hashed_password = _hash_password(password)
+        return self._db.add_user(email, hashed_password)
 
 
 def _hash_password(password: str) -> str:
