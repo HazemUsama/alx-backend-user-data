fffa63bf20b27d62eab25b7e2b868ac21926bac1
HazemUsama <hazemosama681@gmail.com>
2024-07-16 17:24:19 +0300

    Destroy session & Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Simple Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect, url_for
 from auth import Auth
 
 
@@ -40,6 +40,18 @@ def login() -> str:
         response.set_cookie("session_id", session_id)
         return response
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """Logout a user"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('home'))
+    abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,6 +55,11 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session for a User
+        """
+        self._db.update_user(user_id, session_id=None)
+
 
 def _hash_password(password: str) -> str:
     """Hash a password using bcrypt
