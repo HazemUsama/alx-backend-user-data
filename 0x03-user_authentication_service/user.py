8d883c313f23359268969069d0b7eacad85cb99f
HazemUsama <hazemosama681@gmail.com>
2024-07-16 15:04:58 +0300

    fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""User class module"""
+"""User class definition for SQLAlchemy model"""
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -8,17 +8,17 @@ Base = declarative_base()
 
 
 class User(Base):
-    """User class"""
+    """User class definition for SQLAlchemy model"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=False)
-    reset_token = Column(String, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
 
     def __init__(self, email, hashed_password, session_id, reset_token):
-        """Initializes a User object"""
+        """Initializes a new User instance"""
         self.email = email
         self.hashed_password = hashed_password
         self.session_id = session_id
