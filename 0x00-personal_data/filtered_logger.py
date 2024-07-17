e3267039fcce19fe94a2f040aaf08236e7892365
HazemUsama <hazemosama681@gmail.com>
2024-07-17 15:13:17 +0300

    fix annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """filtered_logger module
 """
 import re
+from typing import List
 
 
-def filter_datum(fields: str, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """returns the log message obfuscated
     Args:
